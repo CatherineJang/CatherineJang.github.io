45b1a3bca1218dd30fd2ff66827040486286c7ee
CatherineJang <catherinehjang@gmail.com>
2021-12-27 17:24:53 -0800

    anything change?

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import './App.css';
 
 function App() {
   return (
-      <div>
-          <p> testing </p>
-      </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <p> Website Coming Soon!</p>
-    //   </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+        <p> Website Coming Soon!</p>
+      </header>
+    </div>
   );
 }
 
